40b059d6c4978e99ecd3bcdccff5f0105e39cd08
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-14 22:02:25 +0200

    move middleware to separate files (errorHandler.js, notFoundHandler.js, logger.js) and import them into src/server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import 'dotenv/config';
 import cors from 'cors';
 import helmet from 'helmet';
-import pino from 'pino-http';
-import 'dotenv/config';
 import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 import { Student } from './models/student.js';
 
@@ -11,31 +13,35 @@ const app = express();
 // const PORT = 3000;
 const PORT = process.env.PORT ?? 3000;
 
-// Логування часу
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
-app.use(express.json()); // Middleware для парсингу JSON
-app.use(cors()); // Дозволяє запити з будь-яких джерел
+// Глобальні middleware
+app.use(logger); // 1. Логер першим — бачить усі запити
+app.use(express.json()); // 2. Парсинг JSON-тіла
+app.use(cors()); // 3. Дозвіл для запитів з інших доменів
 app.use(helmet()); //захист
+
+// Логування часу
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
+
+// app.use(
+//   pino({
+//     level: 'info',
+//     transport: {
+//       target: 'pino-pretty',
+//       options: {
+//         colorize: true,
+//         translateTime: 'HH:MM:ss',
+//         ignore: 'pid,hostname',
+//         messageFormat:
+//           '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+//         hideObject: true,
+//       },
+//     },
+//   }),
+// );
+//переносимо в logger.js
 
 // GET /students — список усіх студентів
 app.get('/students', async (req, res) => {
@@ -62,22 +68,30 @@ app.get('/students/:studentId', async (req, res) => {
 // });
 
 // Middleware 404 (після всіх маршрутів)
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+// app.use((req, res) => {
+//   res.status(404).json({ message: 'Route not found' });
+// });
+//переносимо в notFoundHandler.js
 
 // Middleware для обробки помилок
-app.use((err, req, res, next) => {
-  console.error(err);
+// app.use((err, req, res, next) => {
+//   console.error(err);
 
-  const isProd = process.env.NODE_ENV === 'production';
+//   const isProd = process.env.NODE_ENV === 'production';
 
-  res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
-  });
-});
+//   res.status(500).json({
+//     message: isProd
+//       ? 'Something went wrong. Please try again later.'
+//       : err.message,
+//   });
+// });
+//переносимо в errorHandler.js
+
+// 404 — якщо маршрут не знайдено
+app.use(notFoundHandler);
+
+// Error — якщо під час запиту виникла помилка
+app.use(errorHandler);
 
 // підключення до MongoDB
 await connectMongoDB();
