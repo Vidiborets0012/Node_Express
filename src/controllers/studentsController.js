06bd2d7dd109221336d684ee50266fdb8e36a638
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-14 23:37:42 +0200

    add http-errors

--- src/controllers/studentsController.js
+++ src/controllers/studentsController.js
@@ -1,4 +1,5 @@
 import { Student } from '../models/student.js';
+import createHttpError from 'http-errors';
 
 // Отримати список усіх студентів
 export const getStudents = async (req, res) => {
@@ -7,12 +8,19 @@ export const getStudents = async (req, res) => {
 };
 
 // Отримати одного студента за id
-export const getStudentById = async (req, res) => {
+export const getStudentById = async (req, res, next) => {
   const { studentId } = req.params;
   const student = await Student.findById(studentId);
 
+  // if (!student) {
+  //   return res.status(404).json({ message: 'Student not found' });
+  // }
+
+  // Додаємо базову обробку помилки замість res.status(404)
   if (!student) {
-    return res.status(404).json({ message: 'Student not found' });
+    // next(new Error('Student not found'));
+    next(createHttpError('Student not found'));
+    return;
   }
 
   res.status(200).json(student);
