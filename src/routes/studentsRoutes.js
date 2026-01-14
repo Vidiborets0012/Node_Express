3e1a06c79365ce8703d6b82b3b9199fcbbd05610
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-14 22:21:41 +0200

    implement routing organization

---
+++ src/routes/studentsRoutes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { Student } from '../models/student.js';
+
+const router = Router();
+
+// GET /students — список усіх студентів
+router.get('/students', async (req, res) => {
+  const students = await Student.find();
+  res.status(200).json(students);
+});
+
+// GET /students/:studentId — один студент за id
+router.get('/students/:studentId', async (req, res) => {
+  const { studentId } = req.params;
+  const student = await Student.findById(studentId);
+
+  if (!student) {
+    return res.status(404).json({ message: 'Student not found' });
+  }
+
+  res.status(200).json(student);
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -6,8 +6,9 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import studentsRoutes from './routes/studentsRoutes.js';
 
-import { Student } from './models/student.js';
+// import { Student } from './models/student.js';
 
 const app = express();
 // const PORT = 3000;
@@ -19,73 +20,26 @@ app.use(express.json()); // 2. Парсинг JSON-тіла
 app.use(cors()); // 3. Дозвіл для запитів з інших доменів
 app.use(helmet()); //захист
 
-// Логування часу
-// app.use((req, res, next) => {
-//   console.log(`Time: ${new Date().toLocaleString()}`);
-//   next();
+// // GET /students — список усіх студентів
+// app.get('/students', async (req, res) => {
+//   const students = await Student.find();
+//   res.status(200).json(students);
 // });
 
-// app.use(
-//   pino({
-//     level: 'info',
-//     transport: {
-//       target: 'pino-pretty',
-//       options: {
-//         colorize: true,
-//         translateTime: 'HH:MM:ss',
-//         ignore: 'pid,hostname',
-//         messageFormat:
-//           '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-//         hideObject: true,
-//       },
-//     },
-//   }),
-// );
-//переносимо в logger.js
+// // GET /students/:studentId — один студент за id
+// app.get('/students/:studentId', async (req, res) => {
+//   const { studentId } = req.params;
+//   const student = await Student.findById(studentId);
 
-// GET /students — список усіх студентів
-app.get('/students', async (req, res) => {
-  const students = await Student.find();
-  res.status(200).json(students);
-});
+//   if (!student) {
+//     return res.status(404).json({ message: 'Student not found' });
+//   }
 
-// GET /students/:studentId — один студент за id
-app.get('/students/:studentId', async (req, res) => {
-  const { studentId } = req.params;
-  const student = await Student.findById(studentId);
-
-  if (!student) {
-    return res.status(404).json({ message: 'Student not found' });
-  }
-
-  res.status(200).json(student);
-});
-
-// Маршрут для тестування middleware помилки
-// app.get('/test-error', (req, res) => {
-//   // Штучна помилка для прикладу
-//   throw new Error('Something went wrong');
+//   res.status(200).json(student);
 // });
 
-// Middleware 404 (після всіх маршрутів)
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Route not found' });
-// });
-//переносимо в notFoundHandler.js
-
-// Middleware для обробки помилок
-// app.use((err, req, res, next) => {
-//   console.error(err);
-
-//   const isProd = process.env.NODE_ENV === 'production';
-
-//   res.status(500).json({
-//     message: isProd
-//       ? 'Something went wrong. Please try again later.'
-//       : err.message,
-//   });
-// });
-//переносимо в errorHandler.js
+// підключаємо групу маршрутів студента
+app.use(studentsRoutes);
 
 // 404 — якщо маршрут не знайдено
 app.use(notFoundHandler);
