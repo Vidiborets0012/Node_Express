4898b0100580aa64818a7c6a0036d788bfdcfaf0
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-01-19 12:07:11 +0200

    add ID validation to the GET /students/:studentId and DELETE /students/:studentId routes

--- src/routes/studentsRoutes.js
+++ src/routes/studentsRoutes.js
@@ -7,14 +7,25 @@ import {
   deleteStudent,
   updateStudent,
 } from '../controllers/studentsController.js';
-import { createStudentSchema } from '../validations/studentsValidation.js';
+import {
+  createStudentSchema,
+  studentIdParamSchema,
+} from '../validations/studentsValidation.js';
 
 const router = Router();
 
 router.get('/students', getStudents);
-router.get('/students/:studentId', getStudentById);
+router.get(
+  '/students/:studentId',
+  celebrate(studentIdParamSchema),
+  getStudentById,
+);
 router.post('/students', celebrate(createStudentSchema), createStudent);
-router.delete('/students/:studentId', deleteStudent);
+router.delete(
+  '/students/:studentId',
+  celebrate(studentIdParamSchema),
+  deleteStudent,
+);
 router.patch('/students/:studentId', updateStudent);
 
 export default router;
