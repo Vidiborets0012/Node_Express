86eda4cf783fed8b716208db69036622156fd148
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-04 19:44:23 +0200

    add a utility for uploading files - src/utils/saveFileToCloudinary.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,10 @@
 import createHttpError from 'http-errors';
 
 export const updateUserAvatar = async (req, res) => {
+  // 1. Виводимо об'єкт req.file у консоль (термінал)
+  console.log('--- Multer File Object ---');
+  console.log(req.file);
+  console.log('--------------------------');
   if (!req.file) {
     // next(createHttpError(400, 'No file'));
     // return;
