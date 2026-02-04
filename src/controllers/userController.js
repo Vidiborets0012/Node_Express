99722a257703d6223151f01b88f79289e57cd5ef
Vidiborets Viktor <vidiboretsv@gmail.com>
2026-02-04 18:53:15 +0200

    add file existence check to the updateUserAvatar controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,10 @@
-export const updateUserAvatar = async (req, res, next) => {
+import createHttpError from 'http-errors';
+
+export const updateUserAvatar = async (req, res) => {
+  if (!req.file) {
+    // next(createHttpError(400, 'No file'));
+    // return;
+    throw createHttpError(400, 'No file');
+  }
   res.status(200).json({ url: '' });
 };
